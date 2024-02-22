7ed9a7316c8df87d25a781ae3796cf73aa8b236c
TianaAkinwale <tianorway@gmail.com>
2024-02-22 09:02:07 +0100

    homepage ui

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import { BellIcon } from '@heroicons/react/24/outline'
 import { ChatBubbleOvalLeftIcon } from '@heroicons/react/24/outline'
 import { HeartIcon } from '@heroicons/react/24/outline'
 import { ShareIcon } from '@heroicons/react/24/outline'
+import { BookmarkIcon } from '@heroicons/react/24/outline'
 
  /* if there's notification, icon/color changes*/
 const HomePage = ({ homePageElements }) => {
@@ -27,9 +28,10 @@ const HomePage = ({ homePageElements }) => {
           <BellIcon className="h-6 w-6 cursor-pointer "
             onClick={() => navigateTo("/Notifiations")} />
       </div>
-      <div className="m-[10px] ">
 
-        <div className="shadow-md rounded-[25px] px-[14px] py-[14px] my-[20px] ">
+      <div className="m-[7px] text-sm "> {/* homepage body div, change font for pc */}
+
+        <div className="shadow-md rounded-[25px] px-[14px] py-[14px] my-[8px] ">
           <div className="flex flex-row justify-content mb-[8px] ">
             <img className="h-[30px] w-[30px] mr-[15px] rounded-full cursor-pointer "
               onClick={() => navigateTo("/profile")}
@@ -46,16 +48,19 @@ const HomePage = ({ homePageElements }) => {
             src={ImgHolder} 
             alt='Profile Picture'/> 
           <p className='my-[8px] '>{postText}</p>
-          <div className='flex flex-row mt-[15px] '>
-            <ChatBubbleOvalLeftIcon className="h-6 w-6 cursor-pointer " />
-            <p>{commentCount}</p>
-            <HeartIcon className="h-6 w-6 cursor-pointer text-blue-500 " />
+          <div className='flex flex-row justify-between mt-[15px] mx-[20px]'>
+            <div className='flex flex-row '>
+              <ChatBubbleOvalLeftIcon className="h-5 w-5 cursor-pointer " />
+              <p>{commentCount}</p>
+            </div>
+            <HeartIcon className="h-6 w-6 cursor-pointer text-purple-700 fill-current " />
             <p className=' '>{likeCount}</p>{/* use state to change icon when clicked? */}
             <ShareIcon className="h-6 w-6 cursor-pointer " />
+            <BookmarkIcon className="h-6 w-6 cursor-pointer " />
           </div>  
         </div>
 
-        <div className="shadow-md rounded-[25px] px-[14px] py-[14px] my-[20px] ">
+        <div className="shadow-md rounded-[25px] px-[14px] py-[14px] my-[8px] ">
           <div className="flex flex-row justify-content mb-[8px] ">
             <img className="h-[30px] w-[30px] mr-[15px] rounded-full cursor-pointer "
               onClick={() => navigateTo("/profile")}
@@ -66,20 +71,19 @@ const HomePage = ({ homePageElements }) => {
                 <b>{userName}</b>
               </p>
               <p className='order-last cursor-pointer '>{optionsIcon}</p>
-          </div>
-          {/* img should be original size as posted, so no fixed height  */}
-         
+          </div>         
           <p className='my-[8px] '>{postText}</p>
           <div className='flex flex-row mt-[15px] '>
             <ChatBubbleOvalLeftIcon className="h-6 w-6 cursor-pointer " />
             <p>{commentCount}</p>
-            <HeartIcon className="h-6 w-6 cursor-pointer " />
+            <HeartIcon className="h-6 w-6 cursor-pointer text-purple-700 fill-current " />
             <p className=' '>{likeCount}</p>{/* use state to change icon when clicked? */}
             <ShareIcon className="h-6 w-6 cursor-pointer " />
+            <BookmarkIcon className="h-6 w-6 cursor-pointer " />
           </div>  
         </div>
 
-        <div className="shadow-md rounded-[25px] px-[14px] py-[14px] my-[20px] ">
+        <div className="shadow-md rounded-[25px] px-[14px] py-[14px] my-[8px] ">
           <div className="flex flex-row justify-content mb-[8px] ">
             <img className="h-[30px] w-[30px] mr-[15px] rounded-full cursor-pointer "
               onClick={() => navigateTo("/profile")}
@@ -99,55 +103,7 @@ const HomePage = ({ homePageElements }) => {
             <HeartIcon className="h-6 w-6 cursor-pointer " />
             <p className=' '>{likeCount}</p>{/* use state to change icon when clicked? */}
             <ShareIcon className="h-6 w-6 cursor-pointer " />
-          </div>  
-        </div>
-
-        <div className="shadow-md rounded-[25px] px-[14px] py-[14px] my-[20px] ">
-          <div className="flex flex-row justify-content mb-[8px] ">
-            <img className="h-[30px] w-[30px] mr-[15px] rounded-full cursor-pointer "
-              onClick={() => navigateTo("/profile")}
-              src={ImgHolder} 
-              alt='Profile Picture'/>
-              <p className="cursor-pointer "
-                onClick={() => navigateTo("/profile")}>
-                <b>{userName}</b>
-              </p>
-              <p className='order-last cursor-pointer '>{optionsIcon}</p>
-          </div>
-          {/* img should be original size as posted, so no fixed height  */}
-          <img className="h-4/5 w-full rounded-2xl "
-            src={ImgHolder} 
-            alt='Profile Picture'/> 
-          <p className='my-[8px] '>{postText}</p>
-          <div className='flex flex-row mt-[15px] '>
-            <ChatBubbleOvalLeftIcon className="h-6 w-6 cursor-pointer " />
-            <p>{commentCount}</p>
-            <HeartIcon className="h-6 w-6 cursor-pointer " />
-            <p className=' '>{likeCount}</p>{/* use state to change icon when clicked? */}
-            <ShareIcon className="h-6 w-6 cursor-pointer " />
-          </div>  
-        </div>
-        
-        <div className="shadow-md rounded-[25px] px-[14px] py-[14px] my-[20px] ">
-          <div className="flex flex-row justify-content mb-[8px] ">
-            <img className="h-[30px] w-[30px] mr-[15px] rounded-full cursor-pointer "
-              onClick={() => navigateTo("/profile")}
-              src={ImgHolder} 
-              alt='Profile Picture'/>
-              <p className="cursor-pointer "
-                onClick={() => navigateTo("/profile")}>
-                <b>{userName}</b>
-              </p>
-              <p className='order-last cursor-pointer '>{optionsIcon}</p>
-          </div>
-          {/* img should be original size as posted, so no fixed height  */}
-          <p className='my-[8px] '>{postText}</p>
-          <div className='flex flex-row mt-[15px] '>
-            <ChatBubbleOvalLeftIcon className="h-6 w-6 cursor-pointer " />
-            <p>{commentCount}</p>
-            <HeartIcon className="h-6 w-6 cursor-pointer " />
-            <p className=' '>{likeCount}</p>{/* use state to change icon when clicked? */}
-            <ShareIcon className="h-6 w-6 cursor-pointer " />
+            <BookmarkIcon className="h-6 w-6 cursor-pointer " />
           </div>  
         </div>
         
