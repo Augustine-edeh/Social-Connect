fd6c11603aa943659bfa7412c6f513983610f8bd
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-03-14 09:36:37 +0100

    chore(chatWindow): modify chatWindow styling

--- src/components/ui/messages/MessageBubble.jsx
+++ src/components/ui/messages/MessageBubble.jsx
@@ -1,6 +1,6 @@
 const MessageBubble = ({ message }) => {
   return (
-    <div className="ml-auto bg-blue-600 my-5 py-3 px-4 rounded-l-2xl rounded-tr-2xl text-white w-fit max-w-screen-xl lg:max-w-xl">
+    <div className="ml-auto bg-purple-600 my-5 py-3 px-4 rounded-l-2xl rounded-tr-2xl text-white w-fit max-w-screen-xl lg:max-w-xl">
       {message}
     </div>
   );
