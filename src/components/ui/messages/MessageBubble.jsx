f8af9e73ceaef93d7670fbd739dee4229327fd25
Augustine-edeh <info.augustinesedeh@gmail.com>
2024-03-15 10:17:09 +0100

    feat(MessageBubble): align user_1 & user_2 message bubbles to right and left respectively

--- src/components/ui/messages/MessageBubble.jsx
+++ src/components/ui/messages/MessageBubble.jsx
@@ -10,12 +10,22 @@ const MessageBubble = ({ message }) => {
       day: "numeric",
     }
   );
+  // console.log(message.content);
 
   return (
-    <div className="ml-auto bg-purple-600 my-5 pt-3 pb-1 px-4 rounded-l-2xl rounded-tr-2xl text-white w-fit max-w-screen-xl lg:max-w-xl">
-      <p className="text-base">{message.content}</p>
-      <p className="text-xs text-gray-400 mt-2">{formattedTimestamp}</p>
-    </div>
+    <>
+      {message.sender_id === "user_1" ? (
+        <div className="ml-auto bg-purple-600 my-5 pt-3 pb-1 px-4 rounded-l-2xl rounded-tr-2xl text-white w-fit max-w-screen-xl lg:max-w-xl">
+          <p className="text-base">{message.content}</p>
+          <p className="text-xs text-gray-400 mt-2">{formattedTimestamp}</p>
+        </div>
+      ) : (
+        <div className="mr-auto bg-purple-600 my-5 pt-3 pb-1 px-4 rounded-r-2xl rounded-tl-2xl text-white w-fit max-w-screen-xl lg:max-w-xl">
+          <p className="text-base">{message.content}</p>
+          <p className="text-xs text-gray-400 mt-2">{formattedTimestamp}</p>
+        </div>
+      )}
+    </>
   );
 };
 
